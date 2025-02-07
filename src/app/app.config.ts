37ff87df78bb23f85ac9e5ab8d38b0c4ee2b6f0e
patrick valeri <patrick.valeri@free.fr>
2025-02-07 14:54:18 +0100

    Etape12 item 17 Edit disparait très vite

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,19 @@ import { Routes, provideRouter } from '@angular/router';
 
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component'; // 
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component'
 
 import { provideClientHydration } from '@angular/platform-browser';
 
 // 👇
 const routes: Routes = [
+ 
   { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokédex'},//titre de la page ou de l'onglet
+  { path: 'pokemons/edit/:id', component: PokemonEditComponent, title: 'Pokémon'},
   { path: 'pokemons', component: PokemonListComponent,title: 'Pokemons' },//titre de la page ou de l'onglet
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent,title: 'Non trouvé' },
 ];
 
 export const appConfig: ApplicationConfig = {
