89135671d8057df2ca809e04c46aa1419e059b64
patrick valeri <patrick.valeri@free.fr>
2025-02-08 22:29:08 +0100

    Etape12 item 17 apres ng update Angular Language Service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,9 +10,8 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 // 👇
 const routes: Routes = [
- 
-  { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokédex'},//titre de la page ou de l'onglet
   { path: 'pokemons/edit/:id', component: PokemonEditComponent, title: 'Pokémon'},
+  { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokédex'},//titre de la page ou de l'onglet
   { path: 'pokemons', component: PokemonListComponent,title: 'Pokemons' },//titre de la page ou de l'onglet
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent,title: 'Non trouvé' },
